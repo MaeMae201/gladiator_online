b04cd32aba80390a4a80c1d6b4e330438b453c6f
Maegan Jean Avant <mavant@basecampcodingacademy.org>
2017-11-16 09:54:53 -0600

    added photo

--- FrontEndWeb-FinalProject/index.js
+++ FrontEndWeb-FinalProject/index.js
@@ -1,3 +1,21 @@
+var myINV = {
+    'Nike Shoes': '$70.00',
+    'Nike Shorts': '$45.00',
+    'Nike Socks': '$10.00',
+    'Nike Workout Shirts': '$30.00',
+    'Nike Workout Shorts': '$40.00'
+};
+var myJSON = JSON.stringify(myINV);
+// window.location = 'demo_json.php?x=' + myJSON;
+
+function add_item() {}
+
+function add_to_inventory() {}
+
+function purchase() {}
+
+function add_to_shopping_cart() {}
+
 function main() {
     $('#game-over').html(
         'Game Over <i class="fa fa-gamepad" aria-hidden="true"></i>'
