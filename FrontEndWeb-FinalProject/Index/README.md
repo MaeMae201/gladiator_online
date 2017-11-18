02fd8ab827d4cb00b4654af674a25c3aa3797ed8
Maegan Jean Avant <mavant@basecampcodingacademy.org>
2017-11-17 23:32:24 -0600

    fixing bugs

---
+++ FrontEndWeb-FinalProject/Index/README.md
@@ -0,0 +1,8 @@
+# FrontEndWeb-FinalProject
+
+* Buy And Sell Website *
+-User chooses whether to add item to inventory or buy item.
+-User then either adds item to inventory and shows in the inventory, or chooses an item
+to purchase.
+-User should have the ability to buy or sell an item.
+-user should display inventory
---
+++ FrontEndWeb-FinalProject/Index/index.js
@@ -0,0 +1,31 @@
+var PAGE_DATA = {
+    'Nike Shoes': '$70.00',
+    'Nike Socks': '$20.00',
+    'Nike Workout Shirts': '$30.00',
+    'Nike Workout Shorts': '$40.00'
+};
+var myJSON = JSON.stringify(PAGE_DATA);
+
+function user_sells_item() {
+    PAGE_DATA - item.add_to_shopping_cart();
+}
+
+function user_item_in_inv() {}
+
+function purchase() {
+    PAGE_DATA.add_to_shopping_cart();
+}
+
+function add_to_shopping_cart() {}
+
+function Button() {}
+
+function remove_once_bought() {}
+
+function main() {
+    $('#game-over').html(
+        'Game Over <i class="fa fa-gamepad" aria-hidden="true"></i>'
+    );
+}
+
+$(main);
